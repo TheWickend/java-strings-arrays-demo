ee458592c99aef759db6fcb24eaa9ff193257673
TheWickend <themrnekit423@gmail.com>
2019-11-09 20:25:25 +0200

    init

--- src/org/tyaa/javastringsarraysdemo/JavaStringsArraysDemo.java
+++ src/org/tyaa/javastringsarraysdemo/JavaStringsArraysDemo.java
@@ -17,28 +17,34 @@ public class JavaStringsArraysDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Здравствуйте!");
         // Test comment
         // Test comment 2
         
         // Alt + Enter for import
-        /* Scanner sc = new Scanner(System.in);
+         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Input a number then press Enter: ");
+        System.out.println("Выберите первое число: ");
         int x = sc.nextInt();
-        System.out.println("Input the second number then press Enter: ");
+        System.out.println("Выберите второе число: ");
         int y = sc.nextInt();
-        System.out.println("Choose an option: ");
-        System.out.println("1 - concatination");
-        System.out.println("2 - plus");
+        System.out.println("Выберите операцию: ");
+        System.out.println("1 - Минус");
+        System.out.println("2 - Плюс");
+        System.out.println("3 - Умножение");
+        System.out.println("4 - Деление");
         int action = sc.nextInt();
         if (action == 1) {
-            System.out.println(String.valueOf(x) + String.valueOf(y));
+            System.out.println(x - y);
         } else if (action == 2) {
             System.out.println(x + y);
+        } else if (action == 3) {
+            System.out.println(x * y);
+        } else if (action == 4) {
+            System.out.println((double)x / (double)y);
         } else {
-            System.out.println("Incorrect input data!");
-        } */
+            System.out.println("Неверная операция!");
+        }
         
         /* int x = 10;
         for (int i = 0; i < 10; i++) {
@@ -60,19 +66,12 @@ public class JavaStringsArraysDemo {
         String s2 = "Hi " + s1 + "!";
         System.out.println(s2);*/
         
-        int[] intArray = new int[10];
-        intArray[0] = 500;
-        // System.out.println(intArray[0]);
         
-        for (int i = 0; i < intArray.length; i++) {
-            System.out.println(intArray[i]);
         }
         
         /*for (int i : intArray) {
             System.out.println(i);
         }*/
         
-        //Тестовое изменение
+       
     }
-    
-}
